1fe0751f751822f1f8df8e262e10bc8b5509eaeb
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-15 09:15:01 +0100

    Tablas Pablo

--- src/olakasen/kpasa.java
+++ src/olakasen/kpasa.java
@@ -4,7 +4,29 @@ public class kpasa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		tablasPablo();
 
+	}
+	
+	public static void tablasPablo() {
+		
+		int tablas=10;
+		
+		for(int i=1;i<=tablas;i++) {
+			
+			System.out.println("\n----------\n"
+					+ "Tabla del " + i + 
+					"\n----------\n");
+			
+			for(int j = 0; j<10; j++) {
+				
+				System.out.println(i + " * " + j + " = " + i*j);
+				
+			}
+			
+		}
+	
 	}
 
 }
