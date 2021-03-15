1522d1019487c9fa75d09f8d681cf5d436aef9e9
Zther <Zther@Zther-Dsk>
2021-03-15 09:30:44 +0100

    añado mi codigo

--- src/olakasen/kpasa.java
+++ src/olakasen/kpasa.java
@@ -4,8 +4,17 @@ public class kpasa {
 
 	public static void main(String[] args) {
 		
-		
-
+		tablaEze();
 	}
 
+	private static void tablaEze() {
+		String linea="";
+		for (int tabla=1; tabla<=10; tabla++) {
+		linea="";
+			for (int multiplo=1; multiplo<=10; multiplo++) {
+				linea+=(multiplo+"x"+tabla+"= "+multiplo*tabla+"  \t");
+			}
+			System.out.println(linea);
+		}
+	}
 }
